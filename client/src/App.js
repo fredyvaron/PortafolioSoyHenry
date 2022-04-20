3b124dade1ca8e7d70e7ef480eab988aec1b432e
Esteban5957L <4estebanvillalba20@gmail.com>
2022-04-20 17:33:39 -0300

    Primeros archivos

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,15 @@ import { useState, Fragment } from "react";
 import "./App.css";
 /* import "../node_modules/nes.css/css/nes.css"; */
 
+import NavBar from "./components/NavBar/NavBar.jsx"
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Contact from "./components/contact/Contact";
+
 function App() {
   return (
     <div className="allContainer">
       <BrowserRouter>
+      <NavBar />
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="*" element={<h1>Not found</h1>} />
         </Routes>
       </BrowserRouter>
